cd471e165b35c33e7973613b0166d6b3c3791a36
Chris Johnson <christophj@gmail.com>
2014-02-16 23:43:30 -0800

    installed devise

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ group :doc do
 end
 
 gem 'bootstrap-sass'
+gem 'devise', '~> 3.2.2'
 
 group :development, :test do
 	# Use sqlite3 as the database for Active Record
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,7 @@ Pintertest::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+
+  #required for devise
+  config.action_mailer.default_url_options = { :host => 'limitless-sands-6312.herokuapp.com' }
 end

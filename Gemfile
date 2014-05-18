a202002c62601a6e8a05dcdc579d9872cef5f933
Chris Johnson <christophj@gmail.com>
2014-05-17 21:41:45 -0700

    add s3 for heroku

--- Gemfile
+++ Gemfile
@@ -3,6 +3,8 @@ ruby  '2.0.0'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.2'
 
+gem 'aws-sdk', '~>1.40.3'
+
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,5 +79,15 @@ Pintertest::Application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   #required for devise
-  config.action_mailer.default_url_options = { :host => 'limitless-sands-6312.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'sheltered-gorge-4497.herokuapp.com' }
+
+  # Use Amazon S3 for Prod
+  config.paperclip_defaults = {
+      :storage => :s3,
+      :s3_credentials => {
+          :bucket => ENV['S3_BUCKET_NAME'],
+          :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+          :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+      }
+  }
 end
